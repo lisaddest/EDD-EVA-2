b665f79fa80faea117ed633ae8a660f23408a50c
lisaddest <104457963+lisaddest@users.noreply.github.com>
2022-09-30 09:48:39 -0600

    Update EVA2_1_LISTA_SIMPLE.java

--- EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/EVA2_1_LISTA_SIMPLE.java
+++ EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/EVA2_1_LISTA_SIMPLE.java
@@ -20,19 +20,25 @@ public class EVA2_1_LISTA_SIMPLE {
         LISTA miLISTA = new LISTA();
         //int[] arreglo= new int[1000000];
         
-        for (int i = 0; i < 1000000; i++) {  //O(N^2)
+        /*for (int i = 0; i < 1000000; i++) {  //O(N^2)
             miLISTA.add(1000);
         }
         System.out.println("CONTEO DE NODOS: " + miLISTA.tamaLISTA());
+        */
         
+        miLISTA.add(10); //0
+        miLISTA.add(20); //1
+        miLISTA.add(30); //2
+        miLISTA.add(40); //3
+        miLISTA.add(50); //4
+        miLISTA.add(60); //5
         
-        miLISTA.add(10);
-        miLISTA.add(20);
-        miLISTA.add(30);
-        miLISTA.add(40);
-        miLISTA.add(50);
-        miLISTA.add(60);
+        try{
+        miLISTA.insertarEn(70, 0);
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        miLISTA.imprimir();
      //   miLISTA.imprimir();  
     }
-    
 }
